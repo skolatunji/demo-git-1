398f8b66931c9831912c7018304222185cf0fe4c
sosuite <36875487+sosuite@users.noreply.github.com>
2021-07-07 00:52:53 -0400

    added a couple of files

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 
 resource "aws_instance" "jjtech" {
-ami           = "ami-0ab4d1e9cf9a1215a"
+ami           = "ami-0277b52859bac6f4b"
 instance_type = "t2.micro"
 
 tags = {
   Name = "Test-From-TF"
-}
+ }
 }
